1af1a213f444c7fcaec796a250ec174d92c9fee6
Enrique gonzalez <141170095+Enriquegonzalezz@users.noreply.github.com>
2026-01-12 00:46:12 -0400

    bingoelgocho1.0.1

--- src/presentation/components/layout/Header.tsx
+++ src/presentation/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
+
 import { usePathname } from 'next/navigation';
 import { cn } from '@/shared/utils/cn';
 import { Home, PlayCircle, Grid3x3, LogOut } from 'lucide-react';
